d10cf97bc0451ae5257f55c92017f8028b8a30ae
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2021-08-24 16:23:18 -0500

    revert networking

--- Bounce/RenderWindow.h
+++ Bounce/RenderWindow.h
@@ -4,7 +4,6 @@
 #include "Rect.h"
 #include "Text.h"
 #include "Button.h"
-#include "enet/enet.h"
 
 	class RenderWindow {
 	public:
@@ -45,10 +44,6 @@
 		}
 		int center(int a, int b) {
 			return (a - b) / 2;
-		}
-		void sendPacket(ENetPeer* peer, Rect rect) {
-			ENetPacket* packet = enet_packet_create(&rect, sizeof(rect) + 1, ENET_PACKET_FLAG_RELIABLE);
-			enet_peer_send(peer, 0, packet);
 		}
 		void clear(){
 			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
--- Bounce/main.cpp
+++ Bounce/main.cpp
@@ -8,27 +8,15 @@
 #include "Text.h"
 #include "Collision.h"
 #include "Button.h"
-#include "enet/enet.h"
-
-using namespace std::this_thread;
-using namespace std::chrono;
 #undef main //darn sdl main
 int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	TTF_Init();
-	enet_initialize();
-	ENetHost* client;
-	ENetHost* server;
-	client = enet_host_create(NULL, 1, 1, 0, 0);
-	ENetAddress address;
-	ENetPeer* peer;
-	address.port = 1298;
 	float osW = 1280.0f;
 	float osH = 720.0f;
 	float spdX = .1;
 	float spdY = .07;
-	std::string inputText;
 	RenderWindow window("Bounce", 1280, 720);
 	Text Start = window.loadText("Press any key to go back", osW, 700);
 	Text Back = window.loadText("Press Escape to go back", osW, 600);
@@ -41,21 +29,13 @@ int main(int argc, char* argv[]) {
 	Text P1Win = window.loadText("Player One Wins!", osW, 800);
 	Text P2Win = window.loadText("Player Two Wins!", osW, 800);
 	Text Bounce = window.loadText("Bounce", osW, 500);
-	Text MultiplayerText = window.loadText("Multiplayer", osW, 500);
-	Text WaitForConnection = window.loadText("Waiting for a connection...", osW, osH);
-	Text IpText = window.loadText("IP Adress of Server", osW, 700);
-	Text IpText2 = window.loadText("Press Enter when done", osW, 900);
-	Text Ip = window.loadText(inputText, osW, 800);
 	Rect Ball(window.center(1280, 16), window.center(720, 16), 16, 16);
 	Rect P1(50, window.center(720, 96));
 	Rect P2(1214, window.center(720, 96));
 	Button Singleplayer(window.loadTexture("res/sprites/singleplayer.png"), window.center(1280, 102), window.center(720, 52), 102, 52);
-	Button Multiplayer(window.loadTexture("res/sprites/multiplayer.png"), window.center(1280, 102), window.center(820, 52), 102, 52);
-	Button LocalMultiplayer(window.loadTexture("res/sprites/localMultiplayer.png"), window.center(1280, 102), window.center(920, 52), 102, 52);
-	Button Instructions(window.loadTexture("res/sprites/instructions.png"), window.center(1280, 102), window.center(1020, 52), 102, 52);
-	Button Quit(window.loadTexture("res/sprites/quit.png"), window.center(1280, 102), window.center(1120, 52), 102, 52);
-	Button Host(window.loadTexture("res/sprites/host.png"), window.center(1280, 102), window.center(720, 52), 102, 52);
-	Button Join(window.loadTexture("res/sprites/join.png"), window.center(1280, 102), window.center(820, 52), 102, 52);
+	Button LocalMultiplayer(window.loadTexture("res/sprites/localMultiplayer.png"), window.center(1280, 102), window.center(820, 52), 102, 52);
+	Button Instructions(window.loadTexture("res/sprites/instructions.png"), window.center(1280, 102), window.center(920, 52), 102, 52);
+	Button Quit(window.loadTexture("res/sprites/quit.png"), window.center(1280, 102), window.center(1020, 52), 102, 52);
 
 
 	bool gameRunning = true;
@@ -73,14 +53,11 @@ int main(int argc, char* argv[]) {
 				if (Singleplayer.buttonEvents(event) == true) {
 					screen = 1;
 				}
-				if (Multiplayer.buttonEvents(event)) {
-					screen = 6;
-				}
 				if (LocalMultiplayer.buttonEvents(event) == true) {
-					screen = 4;
+					screen = 2;
 				}
 				if (Instructions.buttonEvents(event) == true) {
-					screen = 5;
+					screen = 3;
 				}
 				if (Quit.buttonEvents(event) == true) {
 					gameRunning = false;
@@ -98,97 +75,25 @@ int main(int argc, char* argv[]) {
 				}
 				break;
 			case 2:
-				//server
 				if (event.key.keysym.scancode == SDL_SCANCODE_W && P1.getY() >= 0) {
 					P1.setY(P1.getY() - 6);
 				}
 				if (event.key.keysym.scancode == SDL_SCANCODE_S && P1.getY() <= (osH - P1.getH() + 2)) {
 					P1.setY(P1.getY() + 6);
+				}
+				if (event.key.keysym.scancode == SDL_SCANCODE_UP && P2.getY() >= 0) {
+					P2.setY(P2.getY() - 6);
+				}
+				if (event.key.keysym.scancode == SDL_SCANCODE_DOWN && P2.getY() <= (osH - P2.getH() - 2)) {
+					P2.setY(P2.getY() + 6);
+				}
+				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+					screen = 0;
 				}
 				break;
 			case 3:
-				//client
-				if (event.key.keysym.scancode == SDL_SCANCODE_UP && P2.getY() >= 0) {
-					P2.setY(P2.getY() - 6);
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_DOWN && P2.getY() <= (osH - P2.getH() - 2)) {
-					P2.setY(P2.getY() + 6);
-				}
-				break;
-			case 4:
-				if (event.key.keysym.scancode == SDL_SCANCODE_W && P1.getY() >= 0) {
-					P1.setY(P1.getY() - 6);
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_S && P1.getY() <= (osH - P1.getH() + 2)) {
-					P1.setY(P1.getY() + 6);
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_UP && P2.getY() >= 0) {
-					P2.setY(P2.getY() - 6);
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_DOWN && P2.getY() <= (osH - P2.getH() - 2)) {
-					P2.setY(P2.getY() + 6);
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-					screen = 0;
-				}
-				break;
-			case 5:
 				if (event.type == SDL_KEYDOWN) {
 					screen = 0;
-				}
-				break;
-			case 6:
-				if (Host.buttonEvents(event) == true) {
-					screen = 7;
-				}
-				if (Join.buttonEvents(event) == true) {
-					screen = 8;
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-					screen = 0;
-				}
-				break;
-			case 7: {
-				ENetEvent Eevent;
-				address.host = ENET_HOST_ANY;
-				server = enet_host_create(&address, 1, 1, 0, 0);
-				if (enet_host_service(server, &Eevent, 5000) > 0 && Eevent.type == ENET_EVENT_TYPE_RECEIVE) {
-					screen = 2;
-				}
-			}
-				break;
-			case 8:
-				SDL_StartTextInput();
-				if (event.type == SDL_KEYDOWN) {
-					if (event.key.keysym.sym == SDLK_BACKSPACE && inputText.length() > 0) {
-						inputText.pop_back();
-						renderText = true;
-					}
-					else if (event.key.keysym.sym == SDLK_c && SDL_GetModState() & KMOD_CTRL) {
-						SDL_SetClipboardText(inputText.c_str());
-					}
-					else if (event.key.keysym.sym == SDLK_v && SDL_GetModState() & KMOD_CTRL) {
-						inputText = SDL_GetClipboardText();
-						renderText = true;
-					}
-				}
-				if (event.type == SDL_TEXTINPUT)
-				{
-					if (!(SDL_GetModState() & KMOD_CTRL && (event.text.text[0] == 'c' || event.text.text[0] == 'C' || event.text.text[0] == 'v' || event.text.text[0] == 'V')))
-					{
-						inputText += event.text.text;
-						renderText = true;
-					}
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_RETURN) {
-					ENetEvent Eevent;
-					SDL_StopTextInput();
-					enet_address_set_host(&address, inputText.c_str());
-					peer = enet_host_connect(client, &address, 1, 0);
-					if  (enet_host_service(client, &Eevent, 5000) > 0 && Eevent.type == ENET_EVENT_TYPE_CONNECT) {
-						window.sendPacket(peer, Ball);
-						screen = 2;
-					}
 				}
 				break;
 			}
@@ -212,7 +117,6 @@ int main(int argc, char* argv[]) {
 				P2.scale(sx, sy);
 				Bounce.scale(sx, sy);
 				Singleplayer.scale(sx, sy);
-				Multiplayer.scale(sx, sy);
 				LocalMultiplayer.scale(sx, sy);
 				Instructions.scale(sx, sy);
 				Quit.scale(sx, sy);
@@ -234,7 +138,6 @@ int main(int argc, char* argv[]) {
 			P2.setY(window.center(720, 96));
 			window.renderText(Bounce);
 			window.renderButton(Singleplayer);
-			window.renderButton(Multiplayer);
 			window.renderButton(LocalMultiplayer);
 			window.renderButton(Instructions);
 			window.renderButton(Quit);
@@ -247,7 +150,6 @@ int main(int argc, char* argv[]) {
 				P2.setY(P2.getY() + 6);
 			}
 		case 2:
-		case 4:
 			if (collision(Ball, P1) || collision(Ball, P2)) {
 				spdX *= -1;
 			}
@@ -287,58 +189,10 @@ int main(int argc, char* argv[]) {
 			window.renderRect(P2.getRect());
 			break;
 		case 3:
-			if (Ball.getX() <= 0) {
-				window.clear();
-				window.renderText(GameOver);
-				window.renderText(P2Win);
-				window.display();
-				while (SDL_PollEvent(&event)) {
-					if (event.type == SDL_KEYDOWN) {
-						screen = 0;
-					}
-				}
-				break;
-			}
-			if (Ball.getX() >= osW) {
-				window.clear();
-				window.renderText(GameOver);
-				window.renderText(P1Win);
-				window.display();
-				while (SDL_PollEvent(&event)) {
-					if (event.type == SDL_KEYDOWN) {
-						screen = 0;
-					}
-				}
-			}
-			window.renderText(P1Text);
-			window.renderText(P2Text);
-			window.renderRect(Ball.getRect());
-			window.renderRect(P1.getRect());
-			window.renderRect(P2.getRect());
-			break;
-		case 5:
 			window.renderText(Start);
 			window.renderText(ControlP1);
 			window.renderText(ControlP2);
 			window.renderText(ControlEsc);
-			break;
-		case 6:
-			window.renderText(MultiplayerText);
-			window.renderText(Back);
-			window.renderButton(Host);
-			window.renderButton(Join);
-			break;
-		case 7:
-			window.renderText(WaitForConnection);
-			break;
-		case 8:
-			if(renderText) {
-				Ip = window.loadText(inputText, osW, 800);
-				renderText = false;
-            }
-				window.renderText(IpText);
-				window.renderText(IpText2);
-				window.renderText(Ip);
 			break;
 		}
 		window.display();
